9c132af21b6d2b0e4721b26da3015e35bdcf2262
David Cox <dc.cybersecurity@protonmail.com>
2026-01-25 21:13:49 -0500

    Fix Next.js static export for Cloudflare Pages

--- Projects/cloudpuncher-site/next.config.js
+++ Projects/cloudpuncher-site/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+
   images: {
     unoptimized: true,
   },
+
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
